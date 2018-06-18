d9fa1219f91d77be6c1e3497536007bdbfe431d1
Gilad Takoni <gilad1987@gmail.com>
2018-06-18 09:21:08 +0300

    add TokenInterceptor

---
+++ src/app/auth/token-interceptor.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from "@angular/common/http";
+import {Observable} from "rxjs/index";
+import {AuthService} from "./auth.service";
+import {map, tap} from "rxjs/internal/operators";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TokenInterceptorService implements HttpInterceptor {
+
+    constructor(public auth: AuthService) {
+    }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+        request = request.clone({
+            setHeaders: {
+                Authorization: `Bearer ${this.auth.token}`
+            }
+        });
+
+        return next.handle(request).pipe(tap((event: HttpEvent<any>) => {
+
+            if (event instanceof HttpResponse && event.body.token) {
+                localStorage.setItem('token', event.body.token);
+                console.log(event.body);
+            }
+
+            return event;
+        });
+    }
+}
