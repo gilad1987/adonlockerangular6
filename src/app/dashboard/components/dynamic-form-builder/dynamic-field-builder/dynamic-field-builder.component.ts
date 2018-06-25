186ec0e885aee1d2d12f73fba575df4dfde34785
Gilad Takoni <gilad1987@gmail.com>
2018-06-25 16:42:19 +0300

    add dynamic field builder component

---
+++ src/app/dashboard/components/dynamic-form-builder/dynamic-field-builder/dynamic-field-builder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-field-builder',
+  templateUrl: './dynamic-field-builder.component.html',
+  styleUrls: ['./dynamic-field-builder.component.scss']
+})
+export class DynamicFieldBuilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
