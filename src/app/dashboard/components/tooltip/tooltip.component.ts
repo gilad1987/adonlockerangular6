a7fd76402ec61dcd6ec50fbd6717d68acfccd3d1
Gilad Takoni <gilad1987@gmail.com>
2018-06-26 19:29:41 +0300

    add style to tooltip component

--- src/app/dashboard/components/tooltip/tooltip.component.ts
+++ src/app/dashboard/components/tooltip/tooltip.component.ts
@@ -1,31 +1,68 @@
-import {Component, HostListener, Input, OnInit} from '@angular/core';
+import {
+    AfterContentInit, AfterViewInit, Component, ElementRef, HostListener, Input, OnInit,
+    ViewChild
+} from '@angular/core';
 
 @Component({
     selector: 'app-tooltip',
     templateUrl: './tooltip.component.html',
     styleUrls: ['./tooltip.component.scss']
 })
-export class TooltipComponent implements OnInit {
+export class TooltipComponent implements OnInit, AfterViewInit, AfterContentInit {
+
+    private contentHeight;
+    private contentWidth;
+    private hostWidth;
 
     @Input()
     label;
 
+    @ViewChild('content') content;
+
     @HostListener('mouseenter')
     onMouseEnter(): void {
-        console.log('onMouseEnter');
+        this.content.nativeElement.style.display = 'block';
+        setTimeout(() => {
+            this.content.nativeElement.style.transform = `translate(${(this.contentWidth - this.hostWidth) / 2}px,-${this.contentHeight + 25}px)`;
+            this.content.nativeElement.style.opacity = 1;
+        }, 0);
     }
 
     @HostListener('mouseleave')
     onMouseLeave(): void {
-        console.log('onMouseLeave');
+        this.content.nativeElement.style.transform = `translate(${(this.contentWidth - this.hostWidth) / 2}px,-${this.contentHeight + 5}px)`;
+        this.content.nativeElement.style.opacity = 0;
+        setTimeout(() => {
+            this.content.nativeElement.style.display = 'none';
+        }, 300);
     }
 
-    constructor() {
-    }
 
+    constructor(private element: ElementRef) {
+    }
 
 
     ngOnInit() {
+    }
+
+    ngAfterContentInit() {
+
+        setTimeout(() => {
+            const host = this.element.nativeElement;
+            this.hostWidth = host.offsetWidth;
+            const contentElement = this.content.nativeElement;
+            this.contentWidth = contentElement.offsetWidth;
+            this.contentHeight = contentElement.offsetHeight;
+            // contentElement.style.top = `${height / 2}px`;
+            contentElement.style.transform = `translate(${(this.contentWidth - this.hostWidth) / 2}px,-${this.contentHeight + 5}px)`;
+            // contentElement.style.right = `0px`;
+            contentElement.style.width = `${this.contentWidth}px`;
+            contentElement.style.height = `${this.contentHeight}px`;
+            contentElement.style.position = 'absolute';
+        }, 0);
+    }
+
+    ngAfterViewInit() {
     }
 
 }
