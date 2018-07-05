2ded1a532e5909d9f90edd2fed03dd82e0a7ca72
Gilad takoni <gilad1987@gmail.com>
2018-07-05 22:22:26 +0300

    add toolbar to cabinets component

--- src/app/dashboard/containers/cabinets/cabinets.component.ts
+++ src/app/dashboard/containers/cabinets/cabinets.component.ts
@@ -1,5 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {slideInOutAnimation} from '../../../animations/slide-in-out';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Store} from '../../../services/store/store';
+import {Location} from '@angular/common';
+import {Subscription} from 'rxjs';
 
 @Component({
     selector: 'app-cabinets',
@@ -8,12 +12,37 @@ import {slideInOutAnimation} from '../../../animations/slide-in-out';
     animations: [slideInOutAnimation],
     host: {'[@slideInOutAnimation]': ''}
 })
-export class CabinetsComponent implements OnInit {
+export class CabinetsComponent implements OnInit, OnDestroy {
 
-    constructor() {
+    private routeParamsSub: Subscription;
+
+    public school$: any = this.store.select('school');
+    public section: any;
+
+    constructor(
+        private location: Location,
+        private router: Router,
+        private route: ActivatedRoute,
+        private store: Store) {
     }
 
     ngOnInit() {
+        this.routeParamsSub = this.route.params.subscribe((params) => {
+            console.log(params);
+            this.school$.subscribe((school) => {
+                if (school) {
+                    const sectionId = Number(params.sectionId);
+                    this.section = school.sections.filter((section) => section._id === sectionId)[0];
+                }
+            });
+        });
     }
 
+    goBack() {
+        this.location.back();
+    }
+
+    ngOnDestroy() {
+        this.routeParamsSub.unsubscribe();
+    }
 }
